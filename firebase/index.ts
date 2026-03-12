f9c26f0d8f84ed78b99314a680d2d7516aacbc7c
luckybcharms-create <luckybcharms@gmail.com>
2026-03-12 17:11:27 -0400

    Update index.ts

--- firebase/index.ts
+++ firebase/index.ts
@@ -50,7 +50,7 @@ export function getSdks(firebaseApp: FirebaseApp) {
 }
 
 export * from './provider';
-export * from './client-provider';
+export * from '../components/client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
 export * from './non-blocking-updates';
